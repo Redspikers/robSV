33b6c9ee804c61db84bb5a9588497930babaeba6
redspikers <redspikers@redspikers-NC10.(none)>
2012-03-01 17:17:23 +0100

    Ajout de specification pour Bras

--- Bras.cpp
+++ Bras.cpp
@@ -13,11 +13,39 @@ Bras::Bras(){
 
 bool Bras::attrapeCD()
 {
-    //ok
+	/*
+	* On verifie qu'il est de bonne couleur et on attrape le cd
+	* Si à n'importe quel moment , le CD est laché (et donc le capteur ne voit plus rien)
+	* On retourne False
+	* On retourne True uniquement dans le cas où tout s'est bien passé 
+	* (bonne couleur & pas laché en cours & laché au dessus du tapis et retour position initiale
+	* */
+	
+	
+	
+    //Mouvement des deux servos. Le même tout le temps ?
+    
+    
+    
+    
+    
+    //Compresion de la pompe 
+    pompe.compresser(); 
+    
+    //Mouvement des servos pour aller au tapis
+    
+    //Relachement au dessus du tapis
+    pompe.compresser(); 
+    
+    //Retour position initiale
+    
+    
+    
+    return True;
 }
 
 int Bras::getCapteurCouleur()
 {
-    //ok
+    //return capteur.read(); ?
 }
 
