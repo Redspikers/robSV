99362bbec53b680d1e35e1dd2fb3bb4962d7326a
Victor <redspikers@gmail.com>
2012-03-15 17:35:42 +0100

    Ajout de capteur de distance

--- src/constantes.h
+++ src/constantes.h
@@ -5,6 +5,20 @@
 typedef char position;
 enum{VIDE, OBSTACLE, CD, ROBOT};
 
+//Tableau de conversion entre la valeur du capteur et la distance reelle
+const int capteur_conversion[12][2] = {
+{130, 87},
+{120, 95},
+{110,105},
+{100,115},
+{90, 131},
+{80, 144},
+{70, 170},
+{60, 195},
+{50, 235},
+{40, 296},
+{30, 384},
+{20, 492}};
 
 //Pins : 
 #define PIN_CAPTEUR_COULEUR  1 //Le numero du pin où il y a le capteur (au hasard pour l'instant)
