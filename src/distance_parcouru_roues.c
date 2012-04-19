b36f1b030a4e8f3a2595f3768f08d00e6b6c51be
TItotix <titotix@tiotix-PC.(none)>
2012-04-19 16:13:58 +0200

    textmodif roue codeuse

--- src/distance_parcouru_roues.c
+++ src/distance_parcouru_roues.c
@@ -1,16 +1,36 @@
 // Cette fonction convertit les impulsions reçu des roues codeuse
-// en distance parcouru par les roues en mm
+// en distance parcouru par les roues en mm depuis le début du match
 
 //Paramètre : int compteurImpulsionRoueGauche , int compteurImpulsionRoueDroite (2 variables globales)
 
-// Distance parcourue par chaque roue : pointeur distanceRoueGauche et distanceRoueDroite
+// Distance totale parcourue par chaque roue depuis le début du match : pointeur distanceRoueGauche et distanceRoueDroite
 
 
-void distanceParcouru(int compteurImpulsionRoueGauche, int compteurImpulsionRoueDroite, int *distanceRoueDroite, int *distanceRoueGauche)
+void distanceTotaleParcourue(int compteurImpulsionRoueGauche, int compteurImpulsionRoueDroite, int *distanceTotaleRoueDroite, int *distanceTotaleRoueGauche)
 {
 	
 	*distanceRoueDroite = (compteurImpulsionRoueDroite / 20.25 ) * 189.028 ;
 	*distanceRoueGauche = (compteurImpulsionRoueGauche / 20.25 ) * 189.028 ;
+	
+	
+}
+
+// Convertit le nb d'impulsions des roues codeuse en distancez parcourue
+// par les roues depuis le dernier appel à cette fonction
+
+void distanceParcourue(int compteurImpulsionRoueDroite, int compteurImpulsionRoueGauche, int *distanceRoueDroite, int *distanceRoueGauche) 
+{
+	
+	
+	*distanceRoueDroite = (( compteurImpulsionRoueDroite - *nbImpulsionDroite ) / 20.25 ) * 189.028 ;
+	*distanceRoueGauche = (( compteurImpulsionRoueGauche - *nbImpulsionGauche ) / 20.25 ) * 189.028 ;
+	
+	
+	
+	// nbImpulsion(Gauche et Droite) reçoivent le nb d'impulsion. Ainsi a l' occurence n+1 de cette fonction
+	// ces variables seront le nb d'impulsion lors de l'occurence n de cette fonction
+	*nbImpulsionDroite = compteurImpulsionRoueDroite;
+	*nbImpulsionGauche = compteurImpulsionRoueGauche;
 	
 	
 }
--- src/variableGlobal.h
+++ src/variableGlobal.h
@@ -2,4 +2,4 @@
 #define __VARIABLE__
 
 volatile int compteurImpulsionRoueGauche, compteurImpulsionRoueDroite;
-int *distanceRoueDroite, *distanceRoueGauche;
+int *distanceTotaleRoueDroite, *distanceTotaleRoueGauche, *distanceRoueDroite = 0, *distanceRoueGauche = 0, *nbImpulsionDroite =0, *nbImpulsionGauche = 0;
