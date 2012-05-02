84dc1973dbcb7198131765fe34a26d09566ada70
Titotix <titotix@free.fr>
2012-05-02 15:33:52 +0300

    Update src/Pin.h

--- src/Pin.h
+++ src/Pin.h
@@ -4,20 +4,29 @@
  *  Created on: 1 mai 2012
  *      Author: Koudja
  */
-//hello
+
 
 #ifndef PIN_H_
 #define PIN_H_
 
+// les roues codeuse sont pas définis ici, c'est normal
+// elles sont définis à la dur dans Motor.h parce que c'est legerement différent...
 class Pin {
 	public:
-		static const int CAPTOR = 1;
-		static const int SHOULDER = 2;
-		static const int ELBOW = 3;
-		static const int GATE_LEFT = 4;
-		static const int GATE_RIGHT = 5;
-		static const int POMP = 6;
-		static const int CONVEYOR = 7;
+		static const int CAPTOR = A0;
+		static const int CAPTOR = A1;
+		static const int CAPTOR = A2;
+		static const int CAPTOR = A3;
+		static const int CAPTOR = A4;
+		static const int CAPTOR = A5;
+		static const int CAPTOR = A6;
+		
+		static const int SHOULDER = 8;
+		static const int ELBOW = 9;
+		static const int GATE_LEFT = 11;
+		static const int GATE_RIGHT = 10;
+		static const int POMP = 5;
+		static const int CONVEYOR = 4;
 
 		Pin();
 		virtual ~Pin();
