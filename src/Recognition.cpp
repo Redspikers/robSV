c327f228c2d5d6190f35227f059507d210b31eba
Moi Remoi <claude.perez@utt.fr>
2012-05-08 17:35:23 +0200

    corrections

--- src/Recognition.cpp
+++ src/Recognition.cpp
@@ -38,11 +38,9 @@ Recognition::Recognition() {
 	this->captorTL = new Sensor(Pin::CAPTOR_TOP_LEFT, this->conversion);
 	this->captorTM = new Sensor(Pin::CAPTOR_TOP_MIDDLE, this->conversion);
 	this->captorTR = new Sensor(Pin::CAPTOR_TOP_RIGHT, this->conversion);
-
-	//TODO intégrer le capteur arrière dans les calculs
 	this->captorBack = new Sensor(Pin::CAPTOR_BACK, this->conversion);
 
-	this->valeur = new int[6];
+	this->valeur = new int[7];
 	this->distancesObstacles = new int[9];
 }
 
@@ -50,71 +48,68 @@ void Recognition::analyse() {
 	/*
 	 * ****** TRAITEMENT OBSTACLES ****** (Voir tableau excel "obstacles.xls")
 	 * 0 Si seulement bas gauche compris entre 20 et 50cm : CD gauche
-	 * 1 SI bas Milieu et bas gauche compris entre 20 et 50cm  : CD au milieu Gauche
-	 * 2 Si bas Milieu uniquement compris entre 20 et 50cm  : CD au milieu
-	 * 3 Si seulement bas droite compris entre 20 et 50cm  : CD Droite
-	 * 4 SI bas Milieu et bas droite compris entre 20 et 50cm  : CD au milieu droite
-	 *
-	 * 5 SI haut droit et bas droit compris entre 20 et 150 cm et ~ egales : Obstacle à Droite a Xcm
-	 * 6 SI haut gauche et bas gauche compris entre 20 et 150cm et ~ egales : Obstacle à gauche a Xcm
-	 * 7 Si haut gauche et haut droit et milieu  entre 20 et 49cm : Obstacle devant à Xcm
-	 * 8 Si milieu > à 60cm : Obstacle loin devant
+	 * 1 Si bas Milieu uniquement compris entre 20 et 50cm  : CD au milieu
+	 * 2 Si seulement bas droite compris entre 20 et 50cm  : CD Droite
+	 * 3 SI haut droit et bas droit compris entre 20 et 150 cm et ~ egales : Obstacle à Droite a Xcm
+	 * 4 SI haut gauche et bas gauche compris entre 20 et 150cm et ~ egales : Obstacle à gauche a Xcm
+	 * 5 Si  milieu  entre 20 et 49cm : Obstacle devant à Xcm
+	 * 6 Si arrière entre 20 et 150 cm: Obstacle derrière
 	 */
 
 	//*******OBSTACLE CD*************
 	if (comprisEntre(20, 50, this->valeur[5])) {
 		this->distancesObstacles[0] = this->valeur[5]; //CD GAUCHE
-		if (comprisEntre(20, 50, this->valeur[4])) {
-			this->distancesObstacles[1] = this->valeur[4]; //CD MILIEU GAUCHE
-			this->distancesObstacles[0] = 0; //On supprime CD Gauche car en faite c'est "milieu" gauche
+	}else
+			this->distancesObstacles[0] = 0; //On supprime CD Gauche
 		}
 	}
 
 	//Bas Droit
 	if (comprisEntre(20, 50, this->valeur[3])) {
-		this->distancesObstacles[3] = this->valeur[3]; //CD Droit
-		if (comprisEntre(20, 50, this->valeur[4])) {
-			this->distancesObstacles[4] = this->valeur[4]; //CD MILIEU Droit
-			this->distancesObstacles[3] = 0; //On supprime CD Droit car en faite c'est "milieu" droit
+		this->distancesObstacles[2] = this->valeur[3]; //CD Droit
+		else {
+			this->distancesObstacles[2] = 0; //On supprime CD Droit
 		}
 	}
 
 	//CD MILIEU
-	if (this->distancesObstacles[3] == 0 && this->distancesObstacles[0] == 0 && comprisEntre(20, 50, this->valeur[4])) {
-		this->distancesObstacles[2] = this->valeur[4];
+	if ( comprisEntre(20, 50, this->valeur[4])) {
+		this->distancesObstacles[1] = this->valeur[4];
 	} else {
-		this->distancesObstacles[2] = 0;
+		this->distancesObstacles[1] = 0;
 	}
 
 	//**********AUTRES OBSTACLES*****************
+
+
 	//Haut droit et bas droit
 	if (comprisEntre(20, 150, this->valeur[0]) && comprisEntre(20, 150, this->valeur[3]) && environEgal(this->valeur[3], this->valeur[0], 20)) {
-		this->distancesObstacles[5] = this->valeur[0]; //Obstacle à Droite à Xcm
+		this->distancesObstacles[3] = this->valeur[0]; //Obstacle à Droite à Xcm
 	} else {
-		this->distancesObstacles[5] = 0;
+		this->distancesObstacles[3] = 0;
 	}
 
 	//Haut Gauche et bas Gauche
 	if (comprisEntre(20, 150, this->valeur[1]) && comprisEntre(20, 150, this->valeur[5]) && environEgal(this->valeur[1], this->valeur[5], 20)) {
-		this->distancesObstacles[6] = this->valeur[1]; //Obstacle à Gauche à Xcm
+		this->distancesObstacles[4] = this->valeur[1]; //Obstacle à Gauche à Xcm
+	} else {
+		this->distancesObstacles[4] = 0;
+	}
+
+
+
+	//Milieu
+	if (comprisEntre(20, 150, this->valeur[2])) {
+		this->distancesObstacles[5] = this->valeur[2]; //Obstacle au milieu à Xcm
+	} else {
+		this->distancesObstacles[5] = 0;
+	}
+	// Arrière
+	if (comprisEntre(20, 150, this->valeur[6])) {
+		this->distancesObstacles[6] = this->valeur[6]; //Obstacle à Gauche à Xcm
 	} else {
 		this->distancesObstacles[6] = 0;
 	}
-
-	//Haut Gauche et Haut droit et Milieu
-	if (comprisEntre(20, 50, this->valeur[1]) && comprisEntre(20, 50, this->valeur[0]) && comprisEntre(20, 55, this->valeur[2])) {
-		this->distancesObstacles[7] = this->valeur[2]; //Obstacle à Gauche à Xcm
-	} else {
-		this->distancesObstacles[7] = 0;
-	}
-
-	//Milieu
-	if (comprisEntre(60, 150, this->valeur[2])) {
-		this->distancesObstacles[8] = this->valeur[2]; //Obstacle à Gauche à Xcm
-	} else {
-		this->distancesObstacles[8] = 0;
-	}
-
 }
 
 void Recognition::getValues() {
@@ -124,6 +119,7 @@ void Recognition::getValues() {
 	this->valeur[3] = this->captorBR->get();
 	this->valeur[4] = this->captorBM->get();
 	this->valeur[5] = this->captorBL->get();
+	this->valeur[6] = this->captorBack->get();
 }
 
 bool Recognition::comprisEntre(int min, int max, int value) {
@@ -156,15 +152,14 @@ bool Recognition::environEgal(int value1, int value2, int ecartmax) {
  *
  *Correspondance des indice du tableau "distanceObstacles"
  * 0  CD gauche
- * 1 CD au milieu Gauche
- * 2  CD au milieu
- * 3  CD Droite
- * 4  CD au milieu droite
- * 5  Obstacle à Droite
- * 6  Obstacle à gauche
- * 7  Obstacle devant
- * 8  Obstacle loin devant
+ * 1  CD au milieu
+ * 2  CD Droite
+ * 3  Obstacle à Droite
+ * 4  Obstacle à gauche
+ * 5  Obstacle devant (milieu)
+ * 6  Obstacle derrière
  */
+
 
 int* Recognition::getObstacles() {
 	//Récupération des valeurs
