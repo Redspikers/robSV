a0104900b7998b4140d01ad2add081c02fb4a04e
Victor <redspikers@gmail.com>
2012-05-15 00:13:44 +0200

    Modif Sensor Blue

---
+++ src/SensorConversionBlue.h
@@ -0,0 +1,17 @@
+
+
+#ifndef SENSORCONVERSIONBLUE_H_
+#define SENSORCONVERSIONBLUE_H_
+
+class SensorConversionBlue {
+	public:
+		SensorConversionBlue();
+		~SensorConversionBlue();
+
+		int get(int i, int j);
+
+	private:
+		int** conversion;
+};
+
+#endif /* SENSORCONVERSION_H_ */
